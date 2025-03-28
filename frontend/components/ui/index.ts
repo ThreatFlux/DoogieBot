c5e003ac0038cdd89cab9f472276d7a6e39fcda9
Wyatt Roersma <wyattroersma@gmail.com>
2025-03-28 13:56:48 -0400

    feat: enhance Dockerfile for selective frontend code copying and update component exports
    Signed-off-by: Wyatt Roersma <wyattroersma@gmail.com>

--- frontend/components/ui/index.ts
+++ frontend/components/ui/index.ts
@@ -3,11 +3,9 @@
 // Basic Components
 export { Button } from './Button';
 export { Input } from './Input';
-export { default as Toast } from './Toast';
-export { default as ConfirmDialog } from './ConfirmDialog';
+
 // Use named exports for tooltip components
-export { Tooltip, TooltipTrigger, TooltipContent, TooltipProvider } from './tooltip';
-export { default as SkipLink } from './SkipLink';
-export { default as ProfileDropdown } from './ProfileDropdown';
+export { Tooltip, TooltipTrigger, TooltipContent, TooltipProvider } from './Tooltip';
+
 
 // Add other component exports here as they're created
